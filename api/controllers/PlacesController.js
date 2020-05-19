858ccbbdcfbbc6512e9b61d930a52cc7edb5c3f2
arq-gabo <jgabrielguerral@gmail.com>
2020-05-19 02:12:55 -0500

    [FIX]Protects routes for proprietary users

--- api/controllers/PlacesController.js
+++ api/controllers/PlacesController.js
@@ -8,6 +8,7 @@ function find(req, res, next){
   Place.findOne({slug:req.params.id})
     .then (place => {
       req.place = place;
+      req.mainObj = place;
       next();
     }).catch(err => {
       next(err);
